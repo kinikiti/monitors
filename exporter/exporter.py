3d2f9d30762ee419ce0dec8aa36fab611bffc16e
Kirill Nikitin <knikitin@deloitte.com>
2022-07-15 14:09:54 +0200

    data

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -28,6 +28,22 @@ def get_admin_token(admin_pass='password', cp4d_host='https://ibm-nginx-svc'):
     return str('Bearer ' + admin_token)
 
 
+def convert_cpu_unit(cpu):
+    if 'm' not in cpu:
+        return int(cpu) * 1000
+    return int(cpu[0:-1])
+
+
+def convert_memory_unit(memory):
+    if "Ki" in memory:
+        return int(memory[0:-2]) / 1024 / 1024
+    if "Gi" in memory:
+        return int(memory[0:-2])
+    if "Mi" in memory:
+        return int(memory[0:-2]) / 1024
+    return memory
+
+
 class RandomNumberCollector(object):
     def __init__(self, namespace='cpd', host='https://ibm-nginx-svc'):
         self.namespace = namespace
@@ -71,6 +87,51 @@ class RandomNumberCollector(object):
         active_jobs = CounterMetricFamily("active_jobs_count", "Active jobs at the platform", labels=['activeJobsCount'])
         active_jobs.add_metric(['activeJobsCount'], watsonstudio_active_jobs_overall_count)
         yield active_jobs
+
+        total_runtime = 0
+        events = []
+        for project in projects:
+            project_total_runtime = 0
+            project_total_cpu_limits = 0
+            project_total_memory_limits = 0
+            project_total_cpu_requests = 0
+            project_total_memory_requests = 0
+            project_total_memory_usage = 0
+            project_total_cpu_usage = 0
+
+            labels = 'icpdsupport/projectId={},runtime=true'.format(project['metadata']['guid'])
+            pods = cp4d_monitor.get_pod_usage(label_selector=labels)
+
+            app_labels = 'dsxProjectId={}'.format(project['metadata']['guid'])
+            deployments = cp4d_monitor.get_deployment(label_selector=app_labels)
+
+            if len(pods) > 0:
+                total_runtime += 1
+                for pod in pods:
+                    project_total_runtime = +1
+                    key_deployment = pod['metadata']['name'][0:-14]
+                    deployment_resources = deployments[key_deployment].spec.template.spec.containers[0].resources
+                    pod_cpu_usage = convert_cpu_unit(pod['containers'][1]['usage']['cpu'])
+                    pod_cpu_limits = convert_cpu_unit(deployment_resources.limits['cpu'])
+                    pod_cpu_requests = convert_cpu_unit(deployment_resources.requests['cpu'])
+                    pod_memory_limits = convert_memory_unit(deployment_resources.limits['memory'])
+                    pod_memory_requests = convert_memory_unit(deployment_resources.requests['memory'])
+                    pod_memory_usage = convert_memory_unit(pod['containers'][1]['usage']['memory'])
+
+                    project_total_cpu_limits += pod_cpu_limits
+                    project_total_memory_limits += pod_memory_limits
+                    project_total_cpu_requests += pod_cpu_requests
+                    project_total_memory_requests += pod_memory_requests
+                    project_total_memory_usage += pod_memory_usage
+                    project_total_cpu_usage += project_total_cpu_usage
+
+                    key_deployment = pod['metadata']['name'][0:-14]
+                    deployment_resources = deployments[key_deployment].spec.template.spec.containers[0].resources
+        print(pod_cpu_usage)
+        print(pod_cpu_limits)
+        print(project_total_cpu_limits)
+        print(project_total_memory_limits)
+
 
 if __name__ == "__main__":
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
